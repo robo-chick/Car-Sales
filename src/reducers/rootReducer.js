5c5c80101cd7267faaab802494aa3b8c3ae7c969
Tasha <tashingsworth@gmail.com>
2020-06-19 20:29:47 -0400

    cleaned up code in reducer, took out the filter that was in ADD_FEATURE

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,69 +1,59 @@
-import {ADD_FEATURE, SUBTRACT_FEATURE} from '../actions/rootActions';
-
+import { ADD_FEATURE, SUBTRACT_FEATURE } from '../actions/rootActions';
 
 const initialState = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-}
+
+  additionalPrice: 0,
+  car: {
+    price: 26395,
+    name: '2019 Ford Mustang',
+    image:
+      'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+
+    features: [],
+  },
+  additionalFeatures: [
+    { id: 1, name: 'V-6 engine', price: 1500 },
+    { id: 2, name: 'Racing detail package', price: 1500 },
+    { id: 3, name: 'Premium sound system', price: 500 },
+    { id: 4, name: 'Rear spoiler', price: 250 },
+  ],
+};
 
 export const rootReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case ADD_FEATURE:
-            return {
-                ...state,
-                additionalFeatures: state.additionalFeatures.filter((feature) => {
-                    if(feature !== action.payload) {
-                        return feature
-                    } else {
-                        return null
-                    }
-                }),
+  switch (action.type) {
+    
+    case ADD_FEATURE:
+      // add logic to add a feature from additionalFeatures
+      // reflect updated features in the price
+      return {
+        ...state,
+        additionalPrice: state.additionalPrice + action.payload.price,
+        car: {
+          ...state.car,
+          features: [...state.car.features, action.payload],
+        },
+      };
 
-                car: {
-                    ...state.car,
-                    price: state.car.price + action.payload.price,
-                    features: [...state.car.features, action.payload]
-                },
-                additionalPrice: action.payload.price + state.additionalPrice
-            }
+      case SUBTRACT_FEATURE:
+      // add logic to remove a feature from addedFeatures
+      // reflect updated features in the price
 
-            // remove features from additionalFeatures
-            // add features to car.features
-            // add feature price to car.price
+      return {
+        ...state,
+        additionalPrice: state.additionalPrice - action.payload.price,
 
-        case SUBTRACT_FEATURE:
-            return {
-                ...state,
-                additionalFeatures: [...state.additionalFeatures, action.payload],
-                car: {
-                    ...state.car,
-                    price: state.car.price - action.payload.price,
-                    features: state.car.features.filter(feature => {
-                        if(feature !== action.payload) {
-                            return feature
-                        } else {
-                            return null
-                        }
-                    })
-                },
-                additionalPrice: state.additionalPrice - action.payload.price
-                // add feature to additionalFeatures
-                // remove feature from car.features
-                // remove feature price to car.price
-            }
-            default:
-                return state;
-    }
-}
+        car: {
+          ...state.car,
+          features: [
+            ...state.car.features.filter(
+              feature => feature.id !== action.payload.id
+            ),
+          ],
+        },
+      };
+
+    default:
+      return state;
+  }
+};
+
